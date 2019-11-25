7804f797e422565f9f09e4c09f2019a3e2e08f51
gertruda1 <gertruda1@users.noreply.github.com>
2019-11-25 09:26:23 +0200

    labas

---
+++ app/src/main/java/com/example/wasteyourtime/interactive_fact.java
@@ -0,0 +1,14 @@
+package com.example.wasteyourtime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class interactive_fact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interactive_fact);
+    }
+}
